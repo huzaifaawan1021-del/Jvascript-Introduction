47fb14d3cbd3239b8c0c42769643386172f8f427
Huzaifa <huzaifakhan1021@gmail.com>
2025-10-31 02:55:11 -0700

    JS # 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// alert("Hello and welcome to our website!")
+// alert("Error! Please enter a valid password.")
+// alert("Welcome to JS Land ... n\Happy Coding!")
+// alert("Welcome to JS Land...")
+// alert("Happy coding!")
+// console.log("Hello...I can run JS through my browser's developer console")
+
