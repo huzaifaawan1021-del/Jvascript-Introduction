90c74d1611a8904e5cf53b8c370ad0eae3a13ab4
huzaifaawan1021-del <huzaifaawan1021@gmail.com>
2025-11-07 00:54:44 -0800

    Add files via upload
    Javscript# 2

---
+++ javascript/script.js
@@ -0,0 +1,293 @@
+// // alert("firstname");
+// // alert("lastname");
+// // alert("Email");
+// // alert("Phone Number");
+// // alert("Password");
+// // alert("you're learning jvascript!");
+// // // alert("Welcome to javascript");
+
+// // var countryName;
+// // countryName="Karachi";
+// // console.log(countryName);
+
+// // var firstName;
+// // firstName = "Huzaifa"
+// // document.write(firstName)
+
+// // var teamName = "Pakistan";
+// // alert(teamName)
+
+// // var bestMan = "Charlie";
+// // var bestMan ="Jake";
+
+// // var number = 10;
+// // number = number + 10;
+// // console.log(number);
+
+// // var Merchtotal=100;
+// // var shippingtotal =30;
+// // var ordertotal=Merchtotal+shippingtotal;
+
+
+// // var a = 20;
+// // var b = 10;
+// // var c = b-a;
+// // console.log(c);
+
+// // var bestMan = "Charlie"
+// // bestMan = "Jack"
+
+// // var num;
+// // num = 4+4;
+// // console.log(num);
+
+// // var num= 10;
+// // num = num + 5 ;
+// // console.log(num);
+
+// // var caseqty;
+// // caseqty=144;
+// // var num =  9 ;
+
+// // var num =6000;
+// // var num2= 5 ;
+// // console.log(num*num2);
+// // document.write("Total cost for buy 5 tickets in movie is 30000PKR.")
+
+
+// // document.write("<h2> Rules for naming JS variables </h2>") 
+// // document.write("Variable name must only contain number, $ and _  . For example: $my , user_name and 1stvariable.<br>")
+// // document.write("Variables must begins with a letter, _ or $ For example:$name , _name or name <br>")
+// // document.write("Variables name are case sensitive. <br>")
+// // document.write("Variables should not be JS keywords.<br>")
+
+// // var age,num,name ; 
+
+// // var $name;
+// // var user_name;
+// // var name1;
+// // var firstName;
+// // var _user;
+
+// // var @name;
+// // var user-name;
+// // var user name;
+// // var var;
+// // var 1user;
+
+
+// // document.write(" <h2>Table Of 5 </h2>") 
+// // document.write("5x1=5<br>") 
+// // document.write(" 5x2=10<br>")
+// // document.write(" 5x3=15<br>")
+// // document.write(" 5x4=20<br>")
+// // document.write(" 5x5=25<br>")
+// // document.write(" 5x6=30<br>") 
+// // document.write(" 5x7=35<br>")
+// // document.write(" 5x8=40<br>")
+// // document.write(" 5x9=45<br>")
+// // document.write(" 5x10=50<br>")
+
+// // var num1 = 3;
+// // var num2 = 5;
+// // console.log(num1+num2)
+// // document.write("Sum of 3 and 5 is 8")
+
+
+// // var num1 = 3;
+// // var num2 = 5;
+// // console.log(num1-num2)
+// // document.write("Sum of 3 and 5 is 8")
+
+
+// // var num1 = 3;
+// // var num2 = 5;
+// // console.log(num1*num2)
+// // document.write("Sum of 3 and 5 is 8")
+
+
+// // var num1 = 3;
+// // var num2 = 5;
+// // console.log(num1/num2)
+// // document.write("Sum of 3 and 5 is 8")
+
+
+// // var num1 = 10;
+// // var num2 = 3;
+// // console.log(num1%num2)
+// // document.write("Sum of 3 and 5 is 8")
+
+// //  not completed
+
+// // var num ;
+// // document.write("Value after variable declaration is:?? </br>")
+// // num = 5 ;
+// // document.write(" Initial value = 5 ")
+
+// // var Priceofitem1 = 650;
+// // var Orderedquantityofitem1 = 3;
+// // var Priceofitem2 = 100;
+// // var Orderedquantityofitem2 = 7;
+// // var Shippingcharges = 100;
+// // console.log(Priceofitem1+Orderedquantityofitem1+Priceofitem2+Orderedquantityofitem2+Shippingcharges);
+
+// // document.write("<h2>Shopping Cart</h2> </br>")
+// // document.write("Price of item1 is 650 </br>")
+// // document.write("Quantity of item1 is 3 </br>")
+// // document.write("Quantity of item 2 is 7 </br>")
+// // document.write("Price of item2 is 100 </br>")
+// // document.write("Shipping Charges is 100 </br>")
+// // document.write("Total cost of your order is 2750 ")
+
+// // var totalmarks= 1100;
+// // var obtainedmarks=1079;
+// // console.log(obtainedmarks/totalmarks*100);
+
+// // document.write("Result:</br></br>")
+// // document.write("Total marks of student is 1100</br>")
+// // document.write("Obtained marks of student is 1079</br>")
+// // document.write("Pesrcentage of student is 98.09")
+// // var num = 10 + 5 * 10 / 2; alert(num) ; console.log(num) ; document.write(num)
+
+
+
+
+// // var a;
+// // // console.log(a);
+// // a = 20;
+// // document.writeln("Initial Value " + a);
+// // a++;
+// // document.writeln(" <br> value after increment is " + a)
+// // a+= 9;
+// // document.writeln(" <br> Value after addition of 9 is " + a)
+// // a--;
+// // a -= 12 ;
+// // document.writeln(" <br> Value after decrement  of 12 is " + a)
+// // a = a % 2 ;
+// // console.log(+a);
+
+
+
+
+
+
+// // var a;
+// // console.log(a);
+// // a=20;
+// // document.write(" <br> initial value is" + a )
+// // a++;
+// // document.write(" <br> the value after increment is " + a)
+// // a=a+9;
+// // document.write(" <br> the value after addition is " + a)
+// // a--;
+// // a=a-11;
+// // document.write(" <br> the value after decrement is " + a)
+// // a=a%3;
+// // console.log(+a)
+
+
+
+// // var a = 1;
+// // var b = 2;
+// // var result = --a - --b + ++b + b-- ;
+
+// // var name = prompt("Enter your name");
+// // alert("Hello " + name + " Welcome!");
+
+// // var name = prompt("enter your name");
+// // // alert ("Hi " + name + " Welcome!")
+
+// // var a = 10 ;
+// // document.write(" <br> <h2> Result </h2> " );
+// // document.write(" <br> ---------------------- " );
+// // document.write(" <br> The value of ++a is"  + (++a));
+// // document.write(" <br> Now the value of a is" + a );
+// // document.write(" <br> The value of a++ is" + (a++));
+// // document.write(" <br> Now the value of a is" + a );
+// // document.write(" <br> The value of --a is"  + (--a));
+// // document.write(" <br> Now the value of a is"  + a );
+// // document.write(" <br> The value of a-- is" + (a--));
+// // document.write(" <br> Now the value of a is " + a );
+
+
+// // var celcius = 25;
+// // var fahrenheight =(celcius * 9 / 5 )+32 ;
+// // document.write(celcius + "°C is" + fahrenheight + "°F");
+
+// // var fahrenheight = 70;
+// // var celcius =(fahrenheight - 32)*5/9 ;
+// // document.write( fahrenheight + "°F is" + celcius + "°C");
+
+// // var num = +prompt("enter a number");
+// // num = Number(number);
+// // document.write("<h2>Multiplication Table of"   + num +   "</h2>");
+// // for(var i = 1; i<=10; i++ ){
+// // document.write(num + "X " +  i + "=" + num*1 +  "<br>")
+// // }
+
+// // var currentyear = 2025 ;
+// // var birthyear  = 2008 ;
+// // var yourage= currentyear-birthyear;
+// // // console.log(currentyear-birthyear);
+// // document.write("<h2>Age Calculator</h2> <br>" );
+// // document.write("Current year is : " + currentyear + "<br>" );
+// // document.write("Birth year is : " + birthyear + "<br>") ;
+// // document.write("Your age is : "  + yourage + "<br>") ;
+
+
+
+// var sub1 = prompt("Enter Subject 1 Name:");
+// var sub2 = prompt("Enter Subject 2 Name:");
+// var sub3 = prompt("Enter Subject 3 Name:");
+
+// var totalMarks = 100;
+
+// var o1Marks = +prompt("Enter Obtained Marks for " + sub1 + ":");
+// var o2Marks = +prompt("Enter Obtained Marks for " + sub2 + ":");
+// var o3Marks = +prompt("Enter Obtained Marks for " + sub3 + ":");
+
+// var o1Percentage = o1Marks / totalMarks * 100;
+// var o2Percentage = o2Marks / totalMarks * 100;
+// var o3Percentage = o3Marks / totalMarks * 100;
+
+// var tMarks = totalMarks * 3;
+// var oTMarks = o1Marks + o2Marks + o3Marks;
+// var oBMarks = oTMarks / tMarks * 100;
+
+// document.write(`
+//     <table >
+//             <th>Subject</th>
+//             <th>Total Marks</th>
+//             <th>Obtained Marks</th>
+//             <th>Percentage</th>
+//         </tr>
+
+//         <tr>
+//             <td>${sub1}</td>
+//             <td>${totalMarks}</td>
+//             <td>${o1Marks}</td>
+//             <td>${o1Percentage.toFixed(2)}%</td>
+//         </tr>
+
+//         <tr>
+//             <td>${sub2}</td>
+//             <td>${totalMarks}</td>
+//             <td>${o2Marks}</td>
+//             <td>${o2Percentage.toFixed(2)}%</td>
+//         </tr>
+
+//         <tr>
+//             <td>${sub3}</td>
+//             <td>${totalMarks}</td>
+//             <td>${o3Marks}</td>
+//             <td>${o3Percentage.toFixed(2)}%</td>
+//         </tr>
+
+//         <tr style="font-weight: bold;">
+//             <td></td>
+//             <td>${tMarks}</td>
+//             <td>${oTMarks}</td>
+//             <td>${oBMarks.toFixed(2)}%</td>
+//         </tr>
+//     </table>
+// `);
