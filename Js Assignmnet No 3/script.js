0fda786bcb410defd2971438ca6cbd7dbe2f2fda
huzaifaawan1021-del <huzaifaawan1021@gmail.com>
2025-11-14 04:29:28 -0800

    Add files via upload

---
+++ Js Assignmnet No 3/script.js
@@ -0,0 +1,169 @@
+// var name=prompt("enter your city name?")
+// var city="karachi"
+// if (name===city){
+//     alert("welcome to the city of light!")
+// }
+
+// var gender=prompt("enter your gender?")
+// var gendername="male"
+// if(gender===gendername)  {
+//     alert("Good morning sir!")
+// } else if(gender==="female"){
+//     alert("Good morning ma'am!")
+// }
+
+// var signal=prompt("enter road traffic signal color name?")
+// var color="red"
+// if (signal===color){
+//     alert("Must Stop!")
+// }  else if(signal==="green"){
+//     alert("Move Now!")
+// } else if(signal==="yellow"){
+//     alert("Ready to Move!")
+// }
+
+// var fuel=prompt("How much uel is remaining in your car tell me in liters?")
+// if(fuel< "0.25liter"){
+//     alert("Please refill your fuel!")
+// }
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// 
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// var secretnum =+prompt("Plaese guess secret number!")
+// var num= 5 ;
+// if(secretnum===num){
+//     alert("Bingo!correct answer.")
+// } else if(secretnum===6){
+//     alert("You are close to the right answer!")
+// }
+
+// var num1=+prompt("enter any number of your choice!")
+// if(num1%2===0){
+//     alert("the number is even!")
+// } else{
+//     alert("the number is odd!")
+// }
+
+// var temperature=+prompt("enter your area temperature?")
+// if(temperature>40){
+//     alert("the weather is too hot!")
+// } if (temperature>30){
+//     alert("the weather is normal!")
+// } if(temperature>20){
+//     alert("the weaher is cool!")
+// } if(temperature>10){
+//     alert(" OMG! the weather is too cool .")
+// }
+
+// var num1=+prompt("enter any number?")
+// var num2=+prompt("enter again any number?")
+// var num3=prompt("enter any one operation like (+ , - , / , * and %)")
+// if(num3=="+"){
+//     alert(num1+num2);
+// } else if(num3=="-"){
+//     alert(num1-num2);
+// } else if(num3=="/"){
+//     alert(num1/num2);
+// } else if(num3=="*"){
+//     alert(num1*num2);
+// } else if(num3=="%"){
+//     alert(num1%num2);
+// }
+
+
+
+// var number=+prompt("enter any number")
+// if (number%3===0){
+//     alert( " number is divisible by 3.")
+// }
+
+
+// var a=+prompt("enter any number")
+// var b=+prompt("enter again any number")
+// var c=prompt("enter operators  + - / * %")
+// if(c=="+"){
+//     alert(a+b)
+// } else if(c=="-"){
+//     alert(a-b)
+// } else if(c=="/"){
+//     alert(a/b)
+// } else if (c=="%"){
+//     alert(a%b)
+// } else if(c=="*"){
+//     alert(a*b)
+// }
+
+// var total=+prompt("enter total marks")
+// var obtained=+prompt("enter obt  marks")
+// var tPer = (obtained/total*100)
+// if(tPer>=90 && tPer<=100){
+//     alert("Grade:A-one \n " + "Remarks:Marvellous")
+// } else if(tPer>=80 && tPer<90){
+//     alert("Grage:A+ \n" + "Remarks:Excellent")
+// } else if(tPer>=70 && tPer<80){
+//     alert("Grade:A \n" + "Remarks:Good")
+// } else if(tPer>=60 && tPer<70){
+//     alert("Grade:B \n" + "Remarks:Keep it up")
+// } else if(tPer>=50 && tPer<60){
+//     alert("Grade:C \n" + "Remarks:Need to improve")
+// } else if(tPer>=40 && tPer<50){
+//     alert("Grade:D  \n" + "Remarks: Verypoor")
+// } else if(tPer<40){
+//     alert("Fail  \n" + "Remarks:Try again!") 
+// } else{
+//     alert("something wentwrong!")
+// }
+// document.writeln(`<h1>Marks Sheet:</h1>`)
+// document.writeln(`<b>Your total mark is</b> : ${total} <br>`)
+// document.writeln(`<b>Your obtained total mark is :</b> ${obtained} <br>`)
+// document.writeln(`<b>Your percentage is :</h3></b> ${tPer} %`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
